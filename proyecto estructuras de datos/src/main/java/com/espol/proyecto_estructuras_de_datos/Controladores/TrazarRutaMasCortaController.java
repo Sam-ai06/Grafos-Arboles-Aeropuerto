3c072220375b7d5007f720962d229aa76fb930e8
Samuel Villagomez <sammyvilla3006@gmail.com>
2025-08-24 14:25:08 -0500

    areglado el comboBox y switch
    switch case para el método en base al parámetro elegido en el comboBox

--- proyecto estructuras de datos/src/main/java/com/espol/proyecto_estructuras_de_datos/Controladores/TrazarRutaMasCortaController.java
+++ proyecto estructuras de datos/src/main/java/com/espol/proyecto_estructuras_de_datos/Controladores/TrazarRutaMasCortaController.java
@@ -1,6 +1,7 @@
 package com.espol.proyecto_estructuras_de_datos.Controladores;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -8,7 +9,10 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import modelo_logica.Graph_RedVuelos;
 
-public class TrazarRutaMasCortaController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TrazarRutaMasCortaController implements Initializable {
     @FXML
     private Button btn_trazar, btn_cancelar;
     @FXML
@@ -17,13 +21,19 @@ public class TrazarRutaMasCortaController {
     private Label lbl_msg;
     @FXML
     private ComboBox<String> ComboBox_Opciones;
+
     private Graph_RedVuelos grafo;
 
+
+    public void initialize(URL location, ResourceBundle resources) {
+        initializeComboBox();
+        return;
+    }
+
     //inicializar el comboBox con costo, distancia y tiempo
-    @FXML
-    private void initializeComboBox(){
+    public void initializeComboBox(){
         ComboBox_Opciones.getItems().addAll("Costo", "Distancia", "Tiempo");
-        ComboBox_Opciones.setValue("Elige una opción"); // valor por defecto
+        ComboBox_Opciones.setPromptText("Elige una opción"); // valor por defecto
     }
 
     @FXML
@@ -39,9 +49,26 @@ public class TrazarRutaMasCortaController {
             //usar dijkstra.
             //pensaba en un switch case para las opciones del comboBox, eso depende de como sergio quiera implementarlo
         }*/
-
+        String opcionSeleccionada = ComboBox_Opciones.getValue();
+        switch (opcionSeleccionada) {
+            case "Costo":
+                // Lógica para trazar la ruta más corta basada en costo
+                lbl_msg.setText("Trazando ruta más corta por Costo...");
+                break;
+            case "Distancia":
+                // Lógica para trazar la ruta más corta basada en distancia
+                lbl_msg.setText("Trazando ruta más corta por Distancia...");
+                break;
+            case "Tiempo":
+                // Lógica para trazar la ruta más corta basada en tiempo
+                lbl_msg.setText("Trazando ruta más corta por Tiempo...");
+                break;
+            default:
+                lbl_msg.setText("Por favor, seleccione una opción válida.");
+                break;
+        }
     }
-
+    
     public Graph_RedVuelos getGrafo() {
         return grafo;
     }
